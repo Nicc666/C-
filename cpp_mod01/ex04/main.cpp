11cb10325a9d70ad887222f909d60ccdb1e887ad
Nicc666 <153765746+Nicc666@users.noreply.github.com>
2024-10-18 13:52:20 +0000

    esss4

--- cpp_mod01/ex04/main.cpp
+++ cpp_mod01/ex04/main.cpp
@@ -9,11 +9,32 @@ int main(int argc, char **argv)
         std::cout << "Parameter input error" << std::endl;
         return 1;
     }
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+    std::string line;
     std::ifstream file(argv[1]);
-    if (file.is_open() == true)
-        {
-        std::cout << "Parameter input error" << std::endl;
+    size_t pos;
+    size_t newpos;
+    std::string filenew = argv[1] + "new";
+    std::ofstream outfile(filenew);
+    if (file.is_open() == false)
+    {
+        std::cout << "Opening file error" << std::endl;
         return 1;
     }
+    while (std::getline(file, line))
+    {
+        pos = 0;
+        newpos = 0;
+        while ((newpos = line.find(s1, pos)) != std::string::npos)
+        {
+            outfile << line.substr(pos, newpos - pos);
+            outfile << s2;
+            pos = newpos + s1.length();
+        }
+        outfile << std::endl;
+    }
+    file.close();
+    outfile.close();
     return 0;
 }
