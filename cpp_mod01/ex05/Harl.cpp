7edcf6183fe133a78b57e05dd40679ae7d272f75
Niccolo Spinelli <nspinell@c2r5p9.42firenze.it>
2024-10-19 17:17:59 +0200

    es05

---
+++ cpp_mod01/ex05/Harl.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nspinell <nspinell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 15:44:32 by nspinell          #+#    #+#             */
+/*   Updated: 2024/10/19 17:14:36 by nspinell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+void Harl::debug( void )
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!";
+    std::cout << std::endl;
+    return ;
+}
+
+void Harl::info( void )
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!";
+    std::cout << std::endl;
+    return ;
+}
+
+void Harl::warning( void )
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.";
+    std::cout << std::endl;
+    return ;
+}
+
+void Harl::error( void )
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now.";
+    std::cout << std::endl;
+    return ;
+}
+
+void Harl::complain( std::string level )
+{
+    int i;
+    i = 0;
+    std::string possible[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    if (level != "DEBUG" && level != "INFO" && level != "WARNING" && level != "ERROR")
+    {
+        std::cout << "level error" << std::endl;
+        return ;
+    }
+    void (Harl::*harlPointer[4])(void);
+    harlPointer[0] = &Harl::debug;
+    harlPointer[1] = &Harl::info;
+    harlPointer[2] = &Harl::warning;
+    harlPointer[3] = &Harl::error;
+    while (i < 4)
+    {
+        if (level == possible[i])
+        {
+            (this->*harlPointer[i])();
+            return ;
+        }
+        i++;
+    }
+    return ;
+}
---
+++ cpp_mod01/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nspinell <nspinell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 15:45:25 by nspinell          #+#    #+#             */
+/*   Updated: 2024/10/19 17:11:41 by nspinell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    harl.complain("CANE");
+    return(0);
+}
