b4753668a63ab8e4365a89aa176e37d7ba6eb2ae
Nicc666 <153765746+Nicc666@users.noreply.github.com>
2024-11-16 16:23:16 +0000

    ok1

--- cpp_mod01/ex06/Harl.cpp
+++ cpp_mod01/ex06/Harl.cpp
@@ -21,28 +21,32 @@ Harl::~Harl()
 }
 void Harl::debug( void )
 {
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. \nI really do!";
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. \nI really do!" << std::endl;
     std::cout << std::endl;
     return ;
 }
 
 void Harl::info( void )
 {
-    std::cout << "I cannot believe adding extra bacon costs more money. \nYou didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!";
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. \nYou didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
     std::cout << std::endl;
     return ;
 }
 
 void Harl::warning( void )
 {
-    std::cout << "I think I deserve to have some extra bacon for free. \nI’ve been coming for years whereas you started working here since last month.";
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. \nI’ve been coming for years whereas you started working here since last month." << std::endl;
     std::cout << std::endl;
     return ;
 }
 
 void Harl::error( void )
 {
-    std::cout << "This is unacceptable! I want to speak to the manager now.";
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
     std::cout << std::endl;
     return ;
 }
@@ -63,24 +67,13 @@ void Harl::complain( std::string level )
             lev = i;
         i++;
     }
-    switch (lev)
-    {
-        case 0:
-            std::cout << "[ DEBUG ]" << std::endl;
-            this->debug();
-            std::cout << std::endl;
-        case 1:
-            std::cout << "[ INFO ]" << std::endl;
-            this->info();
-            std::cout << std::endl;
-        case 2:
-            std::cout << "[ WARNING ]" << std::endl;
-            this->warning();
-            std::cout << std::endl;
-        case 3:
-            std::cout << "[ ERROR ]" << std::endl;
-            this->error();
-            std::cout << std::endl;
-    }
+    if (lev == 0)
+        this->debug();
+    if (lev == 0 || lev == 1)
+        this->info();
+    if (lev == 0 || lev == 1 || lev == 2)
+        this->warning();
+    if (lev == 0 || lev == 1 || lev == 2 || lev == 3)
+        this->error();
     return ;
 }
