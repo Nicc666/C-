6abe17ddc4362d4c4626615f2411268ca893df3b
Nicc666 <153765746+Nicc666@users.noreply.github.com>
2024-11-17 22:20:04 +0000

    mod2

---
+++ cpp_mod02/ex00/Fixed.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nspinell <nspinell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 15:44:39 by nspinell          #+#    #+#             */
+/*   Updated: 2024/10/19 17:25:37 by nspinell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <string>
+#include <iostream>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const Fixed &other);
+		~Fixed();
+		Fixed	&operator=(const Fixed &other);
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+	private:
+		int value;
+		static const int bits = 8;
+};
+
+#endif
---
+++ cpp_mod02/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nspinell <nspinell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 15:45:25 by nspinell          #+#    #+#             */
+/*   Updated: 2024/10/19 17:50:23 by nspinell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void )
+{
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+
+    c = b;
+
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
