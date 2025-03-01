40e4d1b60890591b0c782c9a5c2b9fb8d3b83739
Nicc666 <153765746+Nicc666@users.noreply.github.com>
2025-03-01 11:10:06 +0000

    lll

--- exam5/01/Warlock.cpp
+++ exam5/01/Warlock.cpp
@@ -1,6 +1,7 @@
 
 #include "Warlock.hpp"
 
+/*
 Warlock::Warlock()
 {
 }
@@ -17,7 +18,7 @@ Warlock &Warlock::operator=(const Warlock &other)
     name = other.name;
     title = other.title;
     return (*this);
-}
+}*/
 
 Warlock::Warlock(const std::string &name, const std::string &title)
 {
@@ -30,11 +31,12 @@ Warlock::~Warlock()
 {
     std::cout << this->name << ": My job here is done!" << std::endl;
     std::vector<ASpell *>::iterator it = spell.begin();
-    for(; it != spell.end(); it++)
+    for(; it != spell.end(); )
     {
         delete *it;
+        it = spell.erase(it);
     }
-    spell.clear();
+    //spell.clear();
 }
 
 const std::string &Warlock::getName(void) const
@@ -59,20 +61,21 @@ void Warlock::introduce(void) const
 
 void Warlock::learnSpell(ASpell *s)
 {
-    spell.push_back(s);
+    spell.push_back(s->clone());
 }
 
 void Warlock::forgetSpell(std::string spell_name)
 {
     std::vector<ASpell *>::iterator it = spell.begin();
-    for(; it != spell.end(); it++)
+    for(; it != spell.end(); )
     {
         if ((*it)->getName() == spell_name)
         {
             delete *it;
-            spell.erase(it);
-            return;
+            it = spell.erase(it);
         }
+        else
+            it++;
     }
 }
         
--- exam5/01/main.cpp
+++ exam5/01/main.cpp
@@ -5,6 +5,7 @@
 #include "Dummy.hpp"
 #include "Fwoosh.hpp"
 
+/*
 int main()
 {
   Warlock richard("Richard", "the Titled");
@@ -19,4 +20,45 @@ int main()
 
   richard.forgetSpell("Fwoosh");
   richard.launchSpell("Fwoosh", bob);
+}*/
+
+int main()
+{
+    // Creazione di un Warlock
+    Warlock merlin("Merlin", "the Wise");
+    merlin.introduce();
+    std::cout << std::endl;
+
+    // Creazione di un bersaglio Dummy
+    Dummy dummy;
+    
+    // Creazione di incantesimi
+    Fwoosh *fwoosh = new Fwoosh();
+    Fwoosh *fwooshClone = new Fwoosh();
+    
+    // Test: Aggiunta di incantesimi
+    merlin.learnSpell(fwoosh);
+    merlin.learnSpell(fwooshClone); // Dovrebbe evitare duplicati o gestirli correttamente
+    
+    // Test: Lancio dell'incantesimo conosciuto
+    merlin.launchSpell("Fwoosh", dummy);
+    
+    // Test: Rimozione dell'incantesimo
+    merlin.forgetSpell("Fwoosh");
+    merlin.launchSpell("Fwoosh", dummy); // Non dovrebbe lanciare nulla
+    
+    // Test: Rimozione di un incantesimo non esistente
+    merlin.forgetSpell("Fireball"); // Non dovrebbe causare errori
+    
+    // Test: Modifica del titolo del Warlock
+    merlin.setTitle("the Grand Sorcerer");
+    merlin.introduce();
+    
+    // Test: Creazione di un nuovo Warlock e copia
+    Warlock morgana("Morgana", "the Enchantress");
+    morgana.introduce();
+    //morgana = merlin;
+    //morgana.introduce(); // Dovrebbe mostrare le informazioni di Merlin
+    
+    return 0;
 }
